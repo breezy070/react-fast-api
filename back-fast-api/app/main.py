da01c2032c55f68bd32e4d68dba75a8a9e7ff1f6
FabriqueDWWM <fillion.bertrand@gmail.com>
2024-12-07 19:19:56 +0100

    first commit

---
+++ back-fast-api/app/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def read_root():
+    return {"message": "Bienvenue sur FastAPI avec Docker !"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
